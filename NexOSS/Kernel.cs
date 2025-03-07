2e4e21504845afa7849181bf77b3d281de7e556b
Ikhsanovv <ikhsanovbulitt@icloud.com>
2025-03-07 09:19:28 +0300

    добавил игру

--- NexOSS/Kernel.cs
+++ NexOSS/Kernel.cs
@@ -270,11 +270,132 @@ Used RAM: {4}B", CPUbrand, CPUvendor, amount_of_ram, availible_ram, UsedRam);
         }
 
     }
-    class Games
+    public class Games
     {
         internal void Main(object value)
         {
             throw new NotImplementedException();
         }
+            public char[,] board = new char[3, 3];
+
+        public char currentPlayer = 'X'; // используется для отслеживания игрока
+        public void Main(string[] args)
+        {
+            InitializeBoard();
+            bool gameOver = false;
+            // происходит основной игровой процесс
+            while (!gameOver)
+            {
+                DrawBoard();
+                Console.WriteLine($"Игрок {currentPlayer}, ваш ход!");
+                Console.Write("Введите строку (0, 1, 2): ");
+                int row = int.Parse(Console.ReadLine());
+                Console.Write("Введите столбец (0, 1, 2): ");
+                int col = int.Parse(Console.ReadLine());
+                if (IsValidMove(row, col))
+                {
+                    board[row, col] = currentPlayer;
+
+                    if (CheckWin())
+                    {
+                        DrawBoard();
+                        Console.WriteLine($"Игрок {currentPlayer} выиграл!");
+                        gameOver = true;
+                    }
+                    else if (IsBoardFull())
+                    {
+                        DrawBoard();
+                        Console.WriteLine("Ничья!");
+                        gameOver = true;
+                    }
+                    else
+                    {
+                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Недопустимый ход. Попробуйте снова.");
+                }
+
+            }
+        }
+        public void InitializeBoard() // пустое поле в начале игры
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+        public void DrawBoard() // рисует поле в начале игры
+        {
+            Console.WriteLine("  0 1 2");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public bool IsValidMove(int row, int col) // проверяет ход
+        {
+            if (row < 0 || row >= 3 || col < 0 || col >= 3)
+            {
+                return false;
+            }
+            return board[row, col] == ' ';
+        }
+        public bool CheckWin()
+        {
+            // Проверка строк и столбцов
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+                if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+            }
+
+            // Проверка диагоналей
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return true;
+            }
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool IsBoardFull() // проверяет заполненность доски
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
     }
 }
+
+
+
+
